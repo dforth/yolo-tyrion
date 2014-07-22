6c1f2f409b4af93aca60c09f30d47d8a1fc46c69
Don Forth <dforth@codehappy.com>
2014-07-22 13:27:39 -0400

    port issue?

--- server/server-config.js
+++ server/server-config.js
@@ -1,12 +1,12 @@
 module.exports = {
     default: {
-        port: 80,
+        port: 5000,
         data_location: __dirname + '/../data/',
         rest_base_url: '/api/*',
         static_site_root: __dirname + '/../build'
     },
     test: {
-        port: 8080,
+        port: 5000,
         data_location: __dirname + '/../data/',
         rest_base_url: '/api/*',
         static_site_root: __dirname + '/../build'
